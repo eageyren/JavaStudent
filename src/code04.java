c9925752535f20a2514f08f76dd6616ac52ecc68
eageyren <eageyren1211@gmail.com>
2026-08-10 20:34:49 +0800

    更新code04代码，添加.gitignore文件忽略项

--- src/code04.java
+++ src/code04.java
@@ -30,6 +30,18 @@ public class code04 {
         for (int i = 0; i < K; i++) {
             System.out.println(i + "出现的次数" + counts[i]);
         }
+
+        System.out.println("--------------------");
+
+        // 验证g函数的等概率性
+        counts = new int[8];
+        for (int i = 0; i < testTimes; i++) {
+            int num = g();
+            counts[num]++;
+        }
+        for (int i = 1; i < 8; i++) {
+            System.out.println(i + "出现的次数" + counts[i]);
+        }
     }
 
     // 任意的x，x属于[0,1），[0,x)范围上的数出现的概率由原来的x调整为x的平方
@@ -39,12 +51,12 @@ public class code04 {
         // 1 - ( 1 - x ) * 2 , Math.max(Math.random(), Math.random());
     }
 
-    // 等概率返回1~5之间的数,不能改
+    // 等概率返回1~5之间的数，不能改动f1
     public static int f1() {
         return (int) (Math.random() * 5) + 1;
     }
 
-    //等概率返回0和1
+    // 等概率返回0和1
     public static int f2() {
         int ans = 0;
         do {
@@ -53,5 +65,22 @@ public class code04 {
         return ans < 3 ? 0 : 1;
     }
 
-    // 等概率返回1~7之间的数，只能用f1
+    // 等概率返回0~7之间的数
+    public static int f3() {
+        return (f2() << 2) + (f2() << 1) + f2();
+    }
+
+    // 等概率返回0~6之间的数
+    public static int f4() {
+        int ans = 0;
+        do {
+            ans = f3();
+        } while (ans == 7);
+        return ans;
+    }
+
+    // 等概率返回1~7之间的数
+    public static int g() {
+        return f4() + 1;
+    }
 }
